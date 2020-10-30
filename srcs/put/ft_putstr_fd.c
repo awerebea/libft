4378b5ed412333934dc3044a3a43eeee00c27418
awerebea <awerebea.21@gmail.com>
2020-10-30 15:53:09 +0300

    Change the way to check pointer to NULL

--- srcs/put/ft_putstr_fd.c
+++ srcs/put/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: awerebea <awerebea@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/04 12:42:35 by awerebea          #+#    #+#             */
-/*   Updated: 2020/05/09 15:18:18 by awerebea         ###   ########.fr       */
+/*   Updated: 2020/10/30 15:52:26 by awerebea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	if (NULL == s)
+	if (!s)
 		return ;
 	while (*s)
 		ft_putchar_fd(*s++, fd);
